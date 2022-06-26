c98530cd53ea4867ada1fd1c0028c601c7c4904a
MariaDabrowska <merydab@gmail.com>
2022-06-26 12:08:33 +0200

    Pierwsze widoki, poprawki w modelach

--- kostka/models.py
+++ kostka/models.py
@@ -1,15 +1,5 @@
 from django.db import models
-from random import randint
 
 
 class Kostka(models.Model):
     ilosc_scian = models.PositiveSmallIntegerField(primary_key=True)
-
-    def rzut(self):
-        return randint(1, self.ilosc_scian)
-
-    def rzuty_modyfikowane1(self):
-        return (self.rzut() + self.rzut() + self.rzut()) * 5
-
-    def rzuty_modyfikowane2(self):
-        return (self.rzut() + self.rzut() + 6) * 5
