57bf6a971cb7088b0fff24479cd955676338994e
MariaDabrowska <merydab@gmail.com>
2022-06-26 12:08:33 +0200

    Pierwsze widoki, poprawki w modelach

--- kostka/models.py
+++ kostka/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+from random import randint
 
 
 class Kostka(models.Model):
     ilosc_scian = models.PositiveSmallIntegerField(primary_key=True)
+
+    def rzut(self):
+        return randint(1, self.ilosc_scian)
