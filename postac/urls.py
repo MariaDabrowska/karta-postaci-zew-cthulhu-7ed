f95b4aa5edbba633ce07c64a6139934780643daa
MariaDabrowska <merydab@gmail.com>
2022-07-02 09:55:34 +0200

    added urls

--- postac/urls.py
+++ postac/urls.py
@@ -1,4 +1,9 @@
+from django.urls import path
+from postac.views import WidokStworzPostac, WidokStworzCechy, ZastosujWiek
+
 app_name = 'postac'
 urlpatterns = [
-
+    path('stworz-postac/', WidokStworzPostac.as_view(), name='stworz_postac'),
+    path('generuj-cechy/', WidokStworzCechy.as_view(), name='generuj_cechy'),
+    path('zastosuj-wiek/', ZastosujWiek.as_view(), name='zastosuj_wiek'),
 ]
